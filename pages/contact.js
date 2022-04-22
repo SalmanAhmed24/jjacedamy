b4572e7a2638b026d3c7a6b0f75ea3fc9106b76c
Salman Ahmed Abbasis <40638940+SalmanAhmed24@users.noreply.github.com>
2022-04-22 11:55:07 +0500

    added some space

--- pages/contact.js
+++ pages/contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ContactComp from '../components/contact';
 function Contact() {
 	return (
-		<div>
+		<div style={{ marginBottom: '100px' }}>
 			<ContactComp />
 			<div className="elfsight-app-50f6761c-37e0-41cb-bd28-e0b374a61c53" />
 		</div>
